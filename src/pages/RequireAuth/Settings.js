4ba7a196bf87b344bd4591eadd8d086fd02330ba
yf1121 <yfujioka1121@outlook.jp>
2021-07-22 23:08:57 +0900

    upload Account Photo

--- src/pages/RequireAuth/Settings.js
+++ src/pages/RequireAuth/Settings.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Link, Redirect } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import firebase, { db } from "../../settings/firebase";
+import IconButton from "@material-ui/core/IconButton";
+import Avatar from '@material-ui/core/Avatar';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import firebase, { auth, db, storage } from "../../settings/firebase";
 import getQueryStrings from '../../utils/getQueryStrings';
 
 // 保存ボタン
@@ -35,41 +39,106 @@ export default class Settings extends React.Component {
       redirect = queries["redirect"];
       if (queries["skippable"] === "true") skippable = true;
     }
-    this.state = {redirect: decodeURIComponent(redirect), isSkippable: skippable, name: "", avatarUrl: "", displayId: "", ngDisplayId: false, isChanged: false};
+    this.state = {
+      redirect: decodeURIComponent(redirect),
+      isSkippable: skippable,
+      nowname: "",
+      name: "",
+      nowavatarUrl: "",
+      avatarUrl: "",
+      avatarBase64: "",
+      avatarImgType: "",
+      nowdisplayId: "",
+      displayId: "",
+      ngDisplayId: false,
+      isChangedName: 2,
+      isChangedAvatar: 2,
+      isChangedDid: 2,
+      open: false,
+      snackmessage: "",
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
     this.changeName = this.changeName.bind(this);
     this.changeAvatarUrl = this.changeAvatarUrl.bind(this);
     this.changeDisplayId = this.changeDisplayId.bind(this);
     this.updateProfile = this.updateProfile.bind(this);
+    this.alertClose = this.alertClose.bind(this);
+  }
+
+  _isMounted = false;
+
+  componentDidMount() {
+    const user = auth.currentUser;
+    if (user !== null) {
+      this.setState({
+        nowname: user.displayName, name: user.displayName,
+        nowavatarUrl: user.photoURL, avatarUrl: user.photoURL,
+      });
+      db.collection("users").doc(user.uid).get().then((doc) => {
+        this.setState({nowdisplayId: doc.data().displayid, displayId: doc.data().displayid})
+      })
+    }
+    this._isMounted = true;
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   changeName(e) {
-    let changed = true;
-    if (e.target.value === "") changed = false;
-    this.setState({name: e.target.value, isChanged: changed});
+    let changed = 1;
+    if (e.target.value === "") {
+      changed = 0;
+    } else if (e.target.value === this.state.nowname) {
+      changed = 2;
+    }
+    this.setState({name: e.target.value, isChangedName: changed});
   }
 
+  // アバターアイコン画像をアップロードしたとき
   changeAvatarUrl(e) {
-    let changed = true;
-    if (e.target.value === "") changed = false;
-    this.setState({avatarUrl: e.target.value, isChanged: changed});
+    console.log(e.target.files)
+    let photoUrl = this.state.nowavatarUrl;
+    let changed = 1;
+    let type = "";
+    if (e.target.files.length === 0) {
+      changed = 2;
+    } else if (e.target.files[0].size < 1000000 || ["image/png", "image/jpeg"].indexOf(e.target.files[0].type)) {
+      const imageFile = e.target.files[0];
+      photoUrl = URL.createObjectURL(imageFile);
+      type = imageFile.type;
+      const reader = new FileReader();
+      const avatarthis = this;
+      reader.addEventListener("load", function () {
+        // 画像ファイルを base64 文字列に変換
+        avatarthis.setState({avatarBase64: reader.result})
+      }, false);
+      reader.readAsDataURL(imageFile)
+    } else {
+      changed = 0;
+    }
+    this.setState({avatarUrl: photoUrl, avatarImgType: type, isChangedAvatar: changed});
   }
 
   async changeDisplayId(e) {
-    let changed = true;
+    this.setState({displayId: e.target.value});
+    let changed = 1;
     let ngDisplayid = false;
     if (e.target.value.length < 3) {
-      changed = false;
+      changed = 0;
+    } else if (e.target.value === this.state.nowdisplayId) {
+      changed = 2;
     } else {
       await db.collection("displayids").doc(e.target.value).get()
       .then((doc) => {
         if (doc.exists) {
-          changed = false;
+          changed = 0;
           ngDisplayid = true;
         }
       })
     }
-
-    this.setState({displayId: e.target.value, isChanged: changed, ngDisplayId: ngDisplayid});
+    this.setState({isChangedDid: changed, ngDisplayId: ngDisplayid});
   }
 
   async updateProfile() {
@@ -77,15 +146,40 @@ export default class Settings extends React.Component {
     const userDocRef = db.collection("users").doc(user.uid);
 
     let profData = {};
-    if(this.state.name !== "") {
+    // 表示名の更新
+    if(this.state.name !== "" && this.state.name !== this.state.nowname) {
       profData.displayName = this.state.name;
       await userDocRef.update({name: this.state.name});
     }
-    if(this.state.avatarUrl !== "") {
-      profData.photoURL = this.state.avatarUrl;
-      await userDocRef.update({avatarUrl: this.state.avatarUrl});
+    // アバター画像の更新
+    if(this.state.avatarUrl !== "" && this.state.avatarUrl !== this.state.nowavatarUrl) {
+      let filepath = "profile/" + user.uid;
+      if (this.state.avatarImgType === "image/png") {
+        filepath += ".png";
+      } else if (this.state.avatarImgType === "image/jpeg") {
+        filepath += ".jpg";
+      }
+      const storageRef = storage.ref().child(filepath);
+      await storageRef.delete().then(async function() {
+        console.log("現在のプロフィール画像を削除しました");
+      }).catch(function(error) {
+        console.log(error, "現在のプロフィール画像を削除できませんでした");
+      });
+      // BASE64ファイルをアップロード
+      const imgString = this.state.avatarBase64;
+      await storageRef.putString(imgString, "data_url")
+      .then(async function(snapshot) {
+        console.log('Uploaded a blob or file!');
+        await storageRef.getDownloadURL().then(async function (url) {
+          profData.photoURL = url;
+          await userDocRef.update({avatarUrl: url});
+        })
+      }).catch(function(error) {
+        console.log(error, "ファイルをアップできませんでした");
+      });
     }
-    if(this.state.displayId.length >= 3) {
+    // マイページIDの更新
+    if(this.state.isChangedDid === 1) {
       const docRef = db.collection("displayids").doc(this.state.displayId);
       await db.runTransaction((transaction) => {
         return transaction.get(docRef).then(async(sfDoc) => {
@@ -107,33 +201,60 @@ export default class Settings extends React.Component {
 
     user.updateProfile(profData).then(() => {
       console.log("プロフィールを更新しました");
-      this.setState({isChanged: false});
+      this.setState({isChanged: false, isChangedName: 2, isChangedAvatar: 2, isChangedDid: 2, open: true, snackmessage: "プロフィールを更新しました。"});
     }).catch((error) => {
       console.log(error);
+      this.setState({open: true, snackmessage: "問題が発生しました。もう一度お試しください。"});
     }); 
   }
 
+  alertClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState({open: false});
+  };
+
   render() {
+    console.log(this.state)
     return (
       <>
         <p>設定です</p>
+        <IconButton
+          edge="end"
+          className={"settngs-icon"}
+          aria-label="account of current user"
+          aria-haspopup="true"
+          component="label"
+        >
+          <Avatar alt={this.state.nowname} src={this.state.avatarUrl} style={{height:"40px", width:"40px"}} />
+          <input
+            type="file"
+            onChange={this.changeAvatarUrl}
+            style={{ display: "none" }}
+            accept="image/png, image/jpeg"
+          />
+        </IconButton>
         <div className={"inputarea"}>
-          <TextField id="input-displayname" className={"inputsettng"} label="表示名" variant="outlined" onChange={this.changeName} />
+          <TextField id="input-displayname" className={"inputsettng"} label="表示名" variant="outlined" value={this.state.name} onChange={this.changeName} />
         </div>
         <div className={"inputarea"}>
-          <TextField id="input-avatar" className={"inputsettng"} label="アバターURL" variant="outlined" onChange={this.changeAvatarUrl} />
-        </div>
-        <div className={"inputarea"}>
-          <TextField id="input-displayid" className={"inputsettng"} label="マイページアドレス" variant="outlined" onChange={this.changeDisplayId} />
+          <TextField id="input-displayid" className={"inputsettng"} label="マイページアドレス" variant="outlined" value={this.state.displayId} onChange={this.changeDisplayId} />
           <p>
             {this.state.displayId.length >= 3 ?
               this.state.ngDisplayId? "このIDはすでに使用されています" : "このIDを使用できます"
             :
-              ""
+              "IDは3文字以上の半角英数字またはハイフン( - )、アンダースコア( _ )である必要があります"
             }
           </p>
         </div>
-        <SaveBtn handleSave={this.updateProfile} enable={this.state.isChanged} />
+        <SaveBtn
+          handleSave={this.updateProfile}
+          enable={
+            this.state.isChangedName * this.state.isChangedAvatar * this.state.isChangedDid === 0 || this.state.isChangedName * this.state.isChangedAvatar * this.state.isChangedDid === 8?
+            false: true
+          }
+        />
         <Button
           component={Link}
           to={this.state.redirect === "/" ? "/" : decodeURIComponent(this.state.redirect)}
@@ -144,6 +265,24 @@ export default class Settings extends React.Component {
         >
           {this.state.isSkippable? "スキップ" : "キャンセル"}
         </Button>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          open={this.state.open}
+          autoHideDuration={4000}
+          onClose={this.alertClose}
+          message={this.state.snackmessage}
+          action={
+            <React.Fragment>
+              <IconButton size="small" aria-label="close" color="inherit" onClick={this.alertClose}>
+                <CloseIcon fontSize="small" />
+              </IconButton>
+            </React.Fragment>
+          }
+        >
+        </Snackbar>
       </>
     )
   }
